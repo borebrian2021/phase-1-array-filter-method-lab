3445c98353ddfac9e013eb242aa95355b1aed2c3
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-12 14:44:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array,value){
+    return  array.filter( function (user) {
+        return user.toUpperCase() === value.toUpperCase()
+    })
+}
+findMatching(drivers, 'Bobby');
+findMatching( ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'], 'Susan');
+findMatching(drivers, 'Sammy');
+
+
+function fuzzyMatch(drivers,start){
+
+
+    const startsWithSa = drivers.filter((x) => x.toLowerCase().startsWith(start.toLowerCase()));
+    return startsWithSa
+;
+}
+
+fuzzyMatch(['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'],"Sa")
+
+function matchName(drivers,name){
+    const findDriver = drivers.filter((x) => x.name.toLowerCase().startsWith(name.toLowerCase()));
+    return findDriver   
+}
+
+matchName([
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ],"Bobby")
